ed34a6e998caa3e3c3f4174585fd1d5bd56c702a
Jose <marti265@rangers.uwp.edu>
2019-10-07 12:06:12 -0500

    hamburger menu works added swift files to connect the home menu with the the hamburger menu

---
+++ Algae Estimator/HomeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  HomeViewController.swift
+//  Algae Estimator
+//
+//  Created by Jose on 9/29/19.
+//  Copyright © 2019 Software Engineering. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: BaseViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSlideMenuButton()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+class algae_0_1_1_ViewController: BaseViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSlideMenuButton()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
